776e5dc8cbd9e729e8aacd2ecca5a834caefea66
Eric Fritz <eric@eric-fritz.com>
2020-09-16 19:57:08 -0500

    Squash commits.

--- enterprise/cmd/precise-code-intel-worker/main.go
+++ enterprise/cmd/precise-code-intel-worker/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/inconshreveable/log15"
@@ -52,6 +53,8 @@ func main() {
 		Registerer: prometheus.DefaultRegisterer,
 	}
 
+	_ = mustInitializeCodeIntelDatabase()
+
 	store := store.NewObserved(mustInitializeStore(), observationContext)
 	MustRegisterQueueMonitor(observationContext.Registerer, store)
 	workerMetrics := metrics.NewWorkerMetrics(observationContext)
@@ -87,9 +90,25 @@ func mustInitializeStore() store.Store {
 		}
 	})
 
-	if err := dbconn.ConnectToDB(postgresDSN); err != nil {
+	if err := dbconn.SetupGlobalConnection(postgresDSN); err != nil {
 		log.Fatalf("failed to connect to database: %s", err)
 	}
 
 	return store.NewWithHandle(basestore.NewHandleWithDB(dbconn.Global))
+}
+
+func mustInitializeCodeIntelDatabase() *sql.DB {
+	postgresDSN := conf.Get().ServiceConnections.CodeIntelPostgresDSN
+	conf.Watch(func() {
+		if newDSN := conf.Get().ServiceConnections.CodeIntelPostgresDSN; postgresDSN != newDSN {
+			log.Fatalf("detected database DSN change, restarting to take effect: %s", newDSN)
+		}
+	})
+
+	db, err := dbconn.New(postgresDSN, "_codeintel")
+	if err != nil {
+		log.Fatalf("failed to connect to database: %s", err)
+	}
+
+	return db
 }
